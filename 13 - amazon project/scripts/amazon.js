28fe1f591dc4b8e442baf665d36ebe3a31d0e854
gruhmosque01 <garuhmm@gmail.com>
2025-10-09 12:05:16 -0400

    amazon fully modular

--- 13 - amazon project/scripts/amazon.js
+++ 13 - amazon project/scripts/amazon.js
@@ -1,4 +1,5 @@
 import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
 
 products.forEach((product) => {
     const html = `<div class="product-container" id="${product.id}">
