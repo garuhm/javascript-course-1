d75ac17e17c596236fa2537d34262c73c905e585
gruhmosque01 <garuhmm@gmail.com>
2025-10-10 09:49:11 -0400

    test created with jasmine

---
+++ 13 - amazon project/tests-jasmine/moneyTest.js
@@ -0,0 +1,16 @@
+import { formatCurrency } from "../scripts/utils/money.js"
+
+describe("test suite: format currency", () => {
+    it("function works", () => {
+        expect(formatCurrency(2095)).toEqual("20.95")
+    })
+    it("works with 0", () => {
+        expect(formatCurrency(0)).toEqual("0.00")
+    })
+    it("rounds up", () => {
+        expect(formatCurrency(2000.5)).toEqual("20.01")
+    })
+    it("rounds down", () => {
+        expect(formatCurrency(2000.4)).toEqual("20.00")
+    })
+})
